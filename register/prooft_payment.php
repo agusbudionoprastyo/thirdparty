056676e7e1f467ad2af0ec46401564b8f738e912
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2025-01-30 13:52:10 +0700

    Update prooft_payment.php

--- register/prooft_payment.php
+++ register/prooft_payment.php
@@ -18,41 +18,16 @@ if (isset($_FILES['paymentproof'])) {
     $uploadPath = $uploadDir . $fileName;
 
     if (move_uploaded_file($file['tmp_name'], $uploadPath)) {
-        // Update status transaksi
         $sql = "UPDATE transactions SET status = 'paid', payment_prooft = ? WHERE transaction_id = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("ss", $fileName, $transaction_id);
         $stmt->execute();
 
-        // if ($stmt->affected_rows > 0) {
-        //     // Generate password acak 6 angka
-        //     $newPassword = str_pad(rand(0, 999999), 6, '0', STR_PAD_LEFT); // Password 6 angka
-            
-        //     // Update password di tabel users
-        //     $sqlUpdatePassword = "
-        //     UPDATE users u
-        //     JOIN transactions t ON u.id = t.user_id
-        //     SET u.password = ?
-        //     WHERE t.transaction_id = ?;
-        //     ";
-        //     $stmtUpdatePassword = $conn->prepare($sqlUpdatePassword);
-        //     $stmtUpdatePassword->bind_param("ss", $newPassword, $transaction_id);
-        //     $stmtUpdatePassword->execute();
-
-        //     if ($stmtUpdatePassword->affected_rows > 0) {
-        //         echo json_encode([
-        //             'success' => true, 
-        //             'message' => 'Bukti pembayaran berhasil diupload, transaksi diperbarui, dan password pengguna telah diupdate.',
-        //             'newPassword' => $newPassword // Menampilkan password baru yang dihasilkan
-        //         ]);
-        //     } else {
-        //         echo json_encode(['error' => 'Gagal memperbarui password pengguna']);
-        //     }
-
-        //     $stmtUpdatePassword->close();
-        // } else {
-        //     echo json_encode(['error' => 'Gagal memperbarui transaksi']);
-        // }
+        if ($stmt->affected_rows > 0) {
+            echo json_encode(['success' => true, 'message' => 'Bukti pembayaran berhasil diupload dan transaksi diperbarui.']);
+        } else {
+            echo json_encode(['error' => 'Gagal memperbarui transaksi']);
+        }
 
         $stmt->close();
     } else {
