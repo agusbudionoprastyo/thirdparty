b170918a687bbe29affa6e5d9dfcac420a1b826e
agusbudionoprastyo <97543860+agusbudionoprastyo@users.noreply.github.com>
2025-01-30 23:28:51 +0700

    ..

--- admin/get_user.php
+++ admin/get_user.php
@@ -4,15 +4,29 @@ include '../helper/db.php';
 // SQL query to fetch users data
 $sql = "SELECT u.name, u.email, u.phone, u.photo, u.username, u.password, t.transaction_id, t.total_amount, t.payment_method, t.payment_prooft, t.transaction_date, t.status 
         FROM users u
-        JOIN transactions t ON u.id = t.user_id
-        ";
+        JOIN transactions t ON u.id = t.user_id";
 $result = $conn->query($sql);
 
 $users = [];
 if ($result->num_rows > 0) {
     // Loop through all rows and push each to the array
     while($row = $result->fetch_assoc()) {
-        $row['badge_class'] = ($row['status'] === 'Online') ? 'bg-gradient-success' : 'bg-gradient-secondary';
+        // Dynamically set the badge class based on the transaction status
+        switch ($row['status']) {
+            case 'paid':
+                $row['badge_class'] = 'bg-gradient-success';
+                break;
+            case 'pending':
+                $row['badge_class'] = 'bg-gradient-warning';
+                break;
+            case 'confirm':
+                $row['badge_class'] = 'bg-gradient-info';
+                break;
+            default:
+                $row['badge_class'] = 'bg-gradient-secondary';  // Fallback for any other status
+                break;
+        }
+        
         $users[] = $row;
     }
 } else {
